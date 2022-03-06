ddc6b7f0408d998786b90737aa544377c5b44e41
lockness-Ko <videolk9373@gmail.com>
2022-03-06 12:23:32 +1100

    fix routing

--- frontend/index.js
+++ frontend/index.js
@@ -1,16 +1,18 @@
 const express = require('express')
 const app = express()
 // var cors = require('cors')
+const { readFileSync } = require('fs')
 const port = 80
 
 // app.use(cors())
 app.use(express.static('static'))
 
 app.get('/machines', (req, res) => {
-	
+	res.send(readFileSync('static/machines.html'))
 })
 
 app.get('/billing', (req, res) => {
+	res.send(readFileSync('static/billing.html'))
 	
 })
 
