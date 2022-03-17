a3e59b2fba8c0d7a4c787441215eb0bfb6977327
lockness-Ko <videolk9373@gmail.com>
2022-03-17 13:56:47 +1100

    making dind work

--- buzz/run.sh
+++ buzz/run.sh
@@ -2,10 +2,14 @@
 
 npm start &
 
+/usr/local/bin/dockerd &
+
+sleep 30
+
 cd /buzz/containers/fedora
     docker build --no-cache -t buzz/fedora .
 
 cd /buzz/containers/ubuntu
     docker build --no-cache -t buzz/ubuntu .
 
-/usr/local/bin/dockerd
+tail -f /dev/null
